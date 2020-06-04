064e435d1dcf08774250c7d2d16c4a4ae418396d
Bartosz Walicki <walicki.bartosz@gmail.com>
2020-06-04 21:34:32 +0200

    Single stat converted to dummy component.

--- src/app/water-tank/components/single-stat/single-stat.component.ts
+++ src/app/water-tank/components/single-stat/single-stat.component.ts
@@ -8,22 +8,10 @@ import { MeasurmentsService } from '../../services/measurments/measurments.servi
   templateUrl: './single-stat.component.html',
   styleUrls: ['./single-stat.component.scss'],
 })
-export class SingleStatComponent implements OnInit {
-  @Input() tankId: number;
-
-  public singleMeasurment: Observable<LastMeasurmentReponse>;
-
-  constructor(private measurmentService: MeasurmentsService) {}
-
-  public ngOnInit(): void {
-    this.singleMeasurment = this.getData();
-  }
+export class SingleStatComponent {
+  @Input() singleMeasurment$: Observable<LastMeasurmentReponse>;
 
   public roundData(data: number): number {
     return Math.round(data * 100) / 100;
-  }
-
-  private getData(): Observable<LastMeasurmentReponse> {
-    return this.measurmentService.getLastMeasurment(this.tankId);
   }
 }
--- src/app/water-tank/pages/main-stats/main-stats.component.ts
+++ src/app/water-tank/pages/main-stats/main-stats.component.ts
@@ -1,8 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from '@influxdata/influxdb-client/dist/observable';
+import { LastMeasurmentReponse } from 'api/interfaces/last-measurment-response.interface copy';
+import { MeasurmentsService } from '../../services/measurments/measurments.service';
+import { Subject, zip, BehaviorSubject } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-main-stats',
   templateUrl: './main-stats.component.html',
   styleUrls: ['./main-stats.component.scss'],
 })
-export class MainStatsComponent {}
+export class MainStatsComponent implements OnInit {
+  public singleMeasurmentTank0$: Subject<LastMeasurmentReponse>;
+  public singleMeasurmentTank1$: Subject<LastMeasurmentReponse>;
+  public refreshData$: BehaviorSubject<void>;
+
+  constructor(private measurmentService: MeasurmentsService) {
+    this.singleMeasurmentTank0$ = new Subject();
+    this.singleMeasurmentTank1$ = new Subject();
+    this.refreshData$ = new BehaviorSubject(null);
+  }
+
+  public ngOnInit(): void {
+    this.initData();
+  }
+
+  public refreshData(): void {}
+
+  private initData() {
+    this.refreshData$
+      .pipe(
+        switchMap(() => {
+          return zip(this.getSingleMeasurment(0), this.getSingleMeasurment(1));
+        })
+      )
+      .subscribe(
+        ([meas0, meas1]: [LastMeasurmentReponse, LastMeasurmentReponse]) => {
+          this.singleMeasurmentTank0$.next(meas0);
+          this.singleMeasurmentTank1$.next(meas1);
+        }
+      );
+  }
+
+  private getSingleMeasurment(
+    tankId: number
+  ): Observable<LastMeasurmentReponse> {
+    return this.measurmentService.getLastMeasurment(tankId);
+  }
+}
