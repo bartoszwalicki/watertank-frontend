a847be30597f83d7c4ebc930af6a8897b4c95771
Bartosz Walicki <bartosz.walicki@appverk.com>
2020-06-14 00:10:49 +0200

    Better naming and percentage showed by waterlevel.

--- src/app/water-tank/components/single-stat/single-stat.component.ts
+++ src/app/water-tank/components/single-stat/single-stat.component.ts
@@ -9,8 +9,4 @@ import { Observable } from 'rxjs';
 })
 export class SingleStatComponent {
   @Input() singleMeasurment$: Observable<LastMeasurmentReponse>;
-
-  public roundData(data: number): number {
-    return Math.round(data * 100) / 100;
-  }
 }
