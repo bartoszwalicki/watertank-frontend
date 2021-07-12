5605d52b631b9b49b2228b0374164b13d3203ebe
Bartosz Walicki <walicki.bartosz@gmail.com>
2021-07-12 23:13:57 +0200

    New JSN sensor applied, new calibration data.

--- src/app/water-tank/services/measurments/measurments.service.ts
+++ src/app/water-tank/services/measurments/measurments.service.ts
@@ -18,7 +18,9 @@ export class MeasurmentsService {
   public getLastMeasurment(tankId: number): Observable<LastMeasurmentReponse> {
     return this.httpClient
       .get<any>(`/api/lastMetric?tankId=${tankId}`)
-      .pipe(map((lastData) => this.convertSingleDataToPercentage(lastData.data)));
+      .pipe(
+        map((lastData) => this.convertSingleDataToPercentage(lastData.data))
+      );
   }
 
   public getMeasurment(
@@ -27,13 +29,31 @@ export class MeasurmentsService {
   ): Observable<Array<MeasurmentReponse>> {
     return this.httpClient
       .get<any>(`/api/metrics?tankId=${tankId}&timeWindow=${timeWindow}`)
-      .pipe(map((arrayData) => {console.log(arrayData.data); return arrayData.data.map(singleMeasurment => this.convertSingleDataToPercentage(singleMeasurment))}));
+      .pipe(
+        map((arrayData) => {
+          return arrayData.data.map((singleMeasurment) =>
+            this.convertSingleDataToPercentage(singleMeasurment)
+          );
+        })
+      );
   }
 
-  private convertSingleDataToPercentage(meas: LastMeasurmentReponse | MeasurmentReponse): LastMeasurmentReponse {
+  private convertSingleDataToPercentage(
+    meas: LastMeasurmentReponse | MeasurmentReponse
+  ): LastMeasurmentReponse {
     const minTankLevel = 2340;
-    const maxTankLevel = 366;
-    const percentage = Math.floor(((meas.waterLevel - minTankLevel)/(maxTankLevel - minTankLevel)) * 100);
-    return Object.assign({}, {...meas, waterLevel: percentage});
+
+    const maxTankLevel = {
+      tank0: 256,
+      tank1: 293,
+    };
+
+    const percentage = Math.floor(
+      ((meas.waterLevel - minTankLevel) /
+        ((meas.tankId === 0 ? maxTankLevel.tank0 : maxTankLevel.tank1) -
+          minTankLevel)) *
+        100
+    );
+    return Object.assign({}, { ...meas, waterLevel: percentage });
   }
 }
