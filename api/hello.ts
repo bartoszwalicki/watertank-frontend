b70bccded5bcb9f06a8bdea7a404e40233e27763
batoszwalicki <walicki.bartosz@gmail.com>
2022-07-04 14:56:45 +0200

    Test of function outbound.

---
+++ api/hello.ts
@@ -0,0 +1,11 @@
+/**
+ * @param tankId  Tank ID, currently available 0 or 1.
+ *
+ * @returns Last metric
+ */
+
+module.exports = (req, res) => {
+  return res.json({
+    data: 'Hello',
+  });
+};
