6717bf78aab96f6cacfe6c826d0a7167236d57e1
batoszwalicki <walicki.bartosz@gmail.com>
2022-07-16 22:37:12 +0200

    Ng 14.

--- src/app/water-tank/components/tank/tank.component.ts
+++ src/app/water-tank/components/tank/tank.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-tank',
   templateUrl: './tank.component.html',
-  styleUrls: ['./tank.component.scss']
+  styleUrls: ['./tank.component.scss'],
 })
-export class TankComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class TankComponent {
+  constructor() {}
 }
