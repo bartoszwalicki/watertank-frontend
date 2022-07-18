7380269a1374ff89d4ccdf005739170535ed1c5d
batoszwalicki <walicki.bartosz@gmail.com>
2022-07-18 22:51:48 +0200

    API update timeout blind tries.

--- api/measurement.ts
+++ api/measurement.ts
@@ -26,45 +26,36 @@ module.exports = (req: VercelRequest, res: VercelResponse) => {
     `Measurement tank 1 ${tank1MeasMm} mm\n\rMeasurement tank 2 ${tank2MeasMm} mm`
   );
 
-  writeInflux(tank1MeasMm, tank2MeasMm);
-
-  return res.json({
-    status: 'success',
-  });
-};
-
-function writeInflux(watertankMeas1: number, watertankMeas2: number): void {
   const writeApi = new InfluxDB({ url, token, timeout: 8000 }).getWriteApi(
     org,
     bucket,
     WritePrecision.ns
   );
 
+  console.log(url, token, bucket, org);
+
   const point1 = new Point('waterlevel')
     .tag('watertank', '1')
-    .floatField('value', watertankMeas1);
+    .floatField('value', tank1MeasMm);
   writeApi.writePoint(point1);
-
-  writeApi
-    .close()
-    .then(() => {
-      console.log('Data written to influxdb.');
-    })
-    .catch((e) => {
-      console.error(e);
-    });
 
   const point2 = new Point('waterlevel')
     .tag('watertank', '2')
-    .floatField('value', watertankMeas2);
+    .floatField('value', tank2MeasMm);
   writeApi.writePoint(point2);
 
   writeApi
     .close()
     .then(() => {
       console.log('Data written to influxdb.');
+      return res.json({
+        status: 'success',
+      });
     })
     .catch((e) => {
       console.error(e);
+      return res.json({
+        status: 'success',
+      });
     });
-}
+};
